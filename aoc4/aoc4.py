86cddfe9443e2d92120455574fd49d991dfbd38c
Patrick <patrickrank@gmx.de>
2023-12-05 21:06:13 +0100

    aoc4: pt.2

--- aoc4/aoc4.py
+++ aoc4/aoc4.py
@@ -1,6 +1,10 @@
+from functools import reduce
+
 with open("aoc4.txt", "r") as f:
     score = 0
+    score_map = {}
     for line in f.readlines():
+        game_id = int(line.split(":")[0].split(" ")[-1].strip())
         scratchcard = line.split(":")[1].strip()
         winning_numbers = [int(x) for x in scratchcard.split(" | ")[0].split(" ") if x != ""]
         numbers = [int(x) for x in scratchcard.split(" | ")[1].split(" ") if x != ""]
@@ -9,7 +13,14 @@ with open("aoc4.txt", "r") as f:
             if n in winning_numbers:
                 nr_of_wins += 1
         
-        if nr_of_wins > 0:
-            score += 2**(nr_of_wins-1)
+        score_map[game_id] = nr_of_wins
+        
+    count_map = {game_id: 1 for game_id in score_map.keys()}
+    for game_id, score in score_map.items():
+        for i in range(1, score+1):
+            try:
+                count_map[game_id+i] += count_map[game_id] 
+            except:
+                pass
 
-    print(score)
+    print(reduce(lambda x, key: x + count_map[key], count_map, 0))
