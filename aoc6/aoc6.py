c2149df485a0d78c7d4ed2712604cc1567dec202
Patrick <patrickrank@gmx.de>
2023-12-06 20:32:56 +0100

    aoc6: pt.2

--- aoc6/aoc6.py
+++ aoc6/aoc6.py
@@ -1,13 +1,10 @@
 with open("aoc6.txt") as f:
-    time = [int(x.strip()) for x in f.readline().split(":")[1].strip().split(" ") if x!= ""]
-    distance = [int(x.strip()) for x in f.readline().split(":")[1].strip().split(" ") if x!= ""]
+    time = int("".join([x for x in f.readline().split(":")[1].strip().split(" ") if x!= ""]))
+    distance = int("".join([x for x in f.readline().split(":")[1].strip().split(" ") if x!= ""]))
 
-score = 1 
-for i in range(len(time)):
-    nr_of_wins = 0
-    for y in range(1,time[i]):
-        if y * (time[i]-y) > distance[i]:
-            nr_of_wins += 1
-    score *= nr_of_wins
+nr_of_wins = 0
+for i in range(1, time):
+    if i * (time-i) > distance:
+        nr_of_wins += 1
     
-print(score)
+print(nr_of_wins)
