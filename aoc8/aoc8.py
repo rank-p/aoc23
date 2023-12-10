9ef92cb55341af8be27f3a28f3a3e526f845ebca
Patrick Rank <patrickrank@gmx.de>
2023-12-10 16:22:13 +0100

    aoc8: pt2

--- aoc8/aoc8.py
+++ aoc8/aoc8.py
@@ -1,23 +1,34 @@
 import itertools
+import math
+from functools import reduce
 with open("aoc8.txt") as f:
 	text = f.read()
 	instructions , map = text.split("\n\n")
 
 parsed_map = {key: tuple(value[1:-1].split(", ")) for key, value in [x.strip().split(" = ") for x in map.split("\n")][:-1] }	
 #start = map.split("\n\n")[0].split(" = ")[0]
-start = "AAA"
+start = [key for key in parsed_map.keys() if key[-1]=='A'] 
 print(instructions)
 print(start)
 
-steps=0
-for char in itertools.cycle(instructions):
-	if steps % 1000000 == 0: print(steps)
-	if char == 'L':
-		start = parsed_map[start][0]
-	else:
-		start = parsed_map[start][1]
-	steps += 1
-	if start == "ZZZ":
-		break
+
+def findCycle(key, instructions, map):
+	steps=0
+	for char in itertools.cycle(instructions):
+		idx = 0 if char == 'L' else 1
+		key = map[key][idx]
+		steps += 1
+		if key[-1]=='Z' : break
+	return steps, key 
 	
-print(steps)
+
+def lcm(a, b):
+	return abs(a*b) // math.gcd(a,b)
+
+cycle = []
+for key in start:
+	cycle.append(findCycle(key,instructions,parsed_map)[0])
+
+print(reduce(lambda x, y: lcm(x,y), cycle))
+	
+
